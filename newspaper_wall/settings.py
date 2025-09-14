d494b926b87ccf9c579a3f826aff054b0e9e9295
Fan Zui <medmekc94@yandex.ru>
2025-09-14 16:58:23 +0500

    Fix ALLOWED_HOSTS to include zuif.ru domain

--- newspaper_wall/settings.py
+++ newspaper_wall/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['zuif.ru', 'www.zuif.ru', 'localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
